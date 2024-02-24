f5c9491e6c87118aa7f916eaf7e7342069f44b02
jmgalindor9802 <jmgalindor9802@gmail.com>
2024-02-24 09:42:48 -0500

    1 version sin entities

---
+++ src/main/java/com/buildoc/buildocDemo/BuildocDemoApplication.java
@@ -0,0 +1,13 @@
+package com.buildoc.buildocDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildocDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildocDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildoc/buildocDemo/BuildocDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildoc.buildocDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildocDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
